eb7dd6c743129b6ce1aa32a2c147b598fd263bee
teodorstrom <teodorstrom.ts@gmail.com>
2022-04-11 19:46:52 +0000

    add end_game function printing game over, name and total score

--- run.py
+++ run.py
@@ -17,23 +17,25 @@ results = SHEET.worksheet('results')
 
 data = results.get_all_records()
 
-# Define variables where to put name and score from the game to sheet
-row = results.row_values(1)
-col = results.col_values(1)
-cell = results.cell(1,2).value
-
-print(cell)
-
 # Welcome text and rules
 print("-" * 35)
 print("""WELCOME TO MY PYTHON-MADE QUIZ!
 GOOD LUCK!""")
 print("-" * 35)
 
+# End game function
+def end_game():
+    print("-" * 35)
+    print("GAME OVER")
+    print("-" * 35)
+    print(name + ",", "your total score was:", score)
+    print("Thank you for playing this quiz, goodbye!")
+    quit()
+
 
 start_game = input("Type (yes) to start the game! ")
 if start_game != "yes":
-    print("Goodbye!")
+    end_game()
     quit()
 
 score = 0
@@ -41,8 +43,8 @@ name = input("What should we call you? ")
 print(f"Alright {name}, let's play!")
 
 
-# The game starts here
 
+# The game starts here
 
 
 # Add username & score to google sheet
@@ -50,7 +52,6 @@ def add_user_score():
     insertRow = [name, score]
     results.append_row(insertRow, table_range="A1")
 
-addUser = input("Do you want to save your username and score to google sheets? (yes/no) ")
+addUser = input("Do you want to save your name and score to google sheets? (yes/no) ")
 if addUser == "yes":
     add_user_score()
-
